38d17be84e1d26aa031b26eb1374cdc1ddeca334
andermendz <andersonmendz19@gmail.com>
2024-02-15 21:27:54 -0500

    formatted code

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
- import styles from './styles.css'
- import { createProject, loadProjects  } from './project';
+import styles from "./styles.css";
+import { createProject, loadProjects } from "./project";
 
 //  import projects from './project.js'
 
-let createProjectButton = document.getElementById('create-project')
-
+let createProjectButton = document.getElementById("create-project");
 
 createProjectButton.onclick = (e) => {
-e.preventDefault()
-createProject()
-
-}
+  e.preventDefault();
+  createProject();
+};
--- src/tasks.js
+++ src/tasks.js
@@ -1,12 +1,10 @@
 class project {
-
-    constructor(name, description){
-        this.name = name;
-        this.description = description
-    }
+  constructor(name, description) {
+    this.name = name;
+    this.description = description;
+  }
 
   sayHello() {
- console.log('hello, my name is ' + this.name)
-    }
+    console.log("hello, my name is " + this.name);
+  }
 }
-
