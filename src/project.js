723f8e09f7f5158ddc78dbc92e6db7de16ffb748
andermendz <andersonmendz19@gmail.com>
2024-02-20 21:27:22 -0500

    non functional project task displaying with task options

--- src/project.js
+++ src/project.js
@@ -12,16 +12,39 @@ class project {
 
 let projectsJSON = [];
 let projects = [
-  {name: "Project 1", description: "This is project 1", tasks: ["Task 1.1", "Task 1.2"]},
-  {name: "Project 2", description: "This is project 2", tasks: ["Task 2.1", "Task 2.2"]},
-  {name: "Project 3", description: "This is project 3", tasks: ["Task 3.1", "Task 3.2"]},
-  {name: "Project 4", description: "This is project 4", tasks: ["Task 4.1", "Task 4.2"]},
-  {name: "Project 5", description: "This is project 5", tasks: ["Task 5.1", "Task 5.2"]},
-  {name: "Project 6", description: "This is project 6", tasks: ["Task 6.1", "Task 6.2"]}
+  {
+    name: "Project 1",
+    description: "This is project 1",
+    tasks: ["Task 1.1", "Task 1.2"],
+  },
+  {
+    name: "Project 2",
+    description: "This is project 2",
+    tasks: ["Task 2.1", "Task 2.2"],
+  },
+  {
+    name: "Project 3",
+    description: "This is project 3",
+    tasks: ["Task 3.1", "Task 3.2"],
+  },
+  {
+    name: "Project 4",
+    description: "This is project 4",
+    tasks: ["Task 4.1", "Task 4.2"],
+  },
+  {
+    name: "Project 5",
+    description: "This is project 5",
+    tasks: ["Task 5.1", "Task 5.2"],
+  },
+  {
+    name: "Project 6",
+    description: "This is project 6",
+    tasks: ["Task 6.1", "Task 6.2"],
+  },
 ];
 updateJSON();
 loadProjects();
-
 
 export function createProject() {
   let projectInputName = document.getElementById("name");
@@ -46,32 +69,43 @@ export function loadProjects() {
 
   for (let i = 0; i < projects.length; i++) {
     let li = document.createElement("li");
-    let projectPageTitle = document.querySelector('.project-title')
-    let projectPageDescription = document.querySelector('.project-description')
-    let projectPageTasks = document.querySelector('.project-tasks')
+    let projectPageTitle = document.querySelector(".project-title");
+    let projectPageDescription = document.querySelector(".project-description");
+    let projectPageTasks = document.querySelector(".project-tasks");
 
     //clicking on project
     li.onclick = () => {
       projectPageTitle.innerHTML = projects[i].name;
       projectPageDescription.innerHTML = projects[i].description;
       projectPageTasks.innerHTML = "";
-      
-      console.log(li.innerHTML)
 
-      for (let j = 0; j < projects[i].tasks.length; j++){
-        
-        let div = document.createElement("div")
-        div.innerHTML = projects[i].tasks[j];
+      console.log(li.innerHTML);
+
+      // TASKS STRUCTURE
+      for (let j = 0; j < projects[i].tasks.length; j++) {
+        let div = document.createElement("div");
+        div.innerHTML = `
+        <div class="task-content">
+        <div class="task-text">${projects[i].tasks[j]}</div>
+        <div class= " task-options">
+        <input type="checkbox">
+        <span class="material-symbols-outlined">
+delete
+</span>
+        </div>
+        </div>
+      
+       `;
+
         projectPageTasks.appendChild(div);
 
-        console.log(projects[i].tasks[j])
+        console.log(projects[i].tasks[j]);
       }
-    }
+    };
     li.innerHTML = projects[i].name;
     projectsList.appendChild(li);
     console.log(projects[i]);
-    console.log(projectsJSON)
-
+    console.log(projectsJSON);
   }
 
   for (let i = 0; i < projects.length; i++) {
@@ -81,14 +115,11 @@ export function loadProjects() {
   }
 
   // add functionality to projects:
-
 }
 
-function updateJSON (projects) {
+function updateJSON(projects) {
   projectsJSON = JSON.stringify(projects);
-};
-
-
+}
 
 updateJSON(projects);
 
